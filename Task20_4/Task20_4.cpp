503ebff1438678b5c5c30f255700410292302efc
DmityFilchev <filchev_dmitry@mail.ru>
2022-07-16 20:54:18 +0300

    '16'

--- Task20_4/Task20_4.cpp
+++ Task20_4/Task20_4.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include<fstream>
 #include<cstdlib>
+#include<ctime>
 
 int main()
 {
@@ -11,26 +12,13 @@ int main()
     char simbol;
     int sum = 0;
     int num = 0;
+    bool ok = true;
+    std::srand(std::time(nullptr));
     std::cout << "To withdraw cash, enter '-', to deposit '+' : ";
     std::cin >> simbol;
     
     if (simbol == '+')
     {
-        std::ifstream bank("bank.bin", std::ios::binary);
-            if (!bank.is_open())
-            {
-                std::cout << "File not open" << std::endl;
-            }
-            else
-            {
-                for (int i = 0; i < 6; i++)
-                {
-                    bank.read((char*)&rub[i], sizeof(rub[i]));
-                        num += rub[i];
-                }
-            }
-        bank.close();
-
         std::ofstream bankIn("bank.bin", std::ios::binary);
 
         for (int i = 0; i < 6; i++)
@@ -43,38 +31,62 @@ int main()
                 rub[rand() % 6] += 1000 - num;
             }
         }
+        std::cout << "ATM is full\n";
         bankIn.close();
     }
     else if (simbol =='-')
     {
         std::cout << "Enter the amount : ";
         std::cin >> sum;
-        std::ifstream bank("bank.bin", std::ios::binary);
-        if (!bank.is_open())
+        if (sum % 100 == 0)
         {
-            std::cout << "File not open" << std::endl;
+            std::ifstream bank("bank.bin", std::ios::binary);
+            if (!bank.is_open())
+            {
+                std::cout << "File not open" << std::endl;
+            }
+            else
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    bank.read((char*)&rub[i], sizeof(rub[i]));
+                }
+                bank.close();
+                std::ofstream bankOut("bank.bin", std::ios::binary);
+                for (int i = 0; i < 6; i++)
+                {
+                    breakdown[i] += (sum / cup[i]);
+                    sum = sum % cup[i];
+                    if (rub[i] - breakdown[i] < 0)
+                    {
+                        int t = breakdown[i] - rub[i];
+                        breakdown[i] -= t;
+                        sum += (t * cup[i]);
+                    }
+                    if (i == 5 && sum > 0)
+                    {
+                        std::cout << "This amount cannot be issued!!!!\n";
+                        rub[i] += breakdown[i];
+                        ok = false;
+                    }
+                    rub[i] -= breakdown[i];
+                    bankOut.write((char*)&rub[i], sizeof(rub[i]));
+                }
+                if (ok)
+                {
+                    std::cout << "Amount issued\n";
+                    for (int i = 0; i < 6; i++)
+                    {
+                        if (breakdown[i] != 0)
+                        {
+                            std::cout << breakdown[i] << " banknotes in face value " << cup[i] << std::endl;
+                        }
+                    }
+                }
+                bankOut.close();
+            }
         }
         else
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                bank.read((char*)&rub[i], sizeof(rub[i]));
-            }
-            for (int i = 0; i < 6; i++)
-            {
-                breakdown[i] += (sum / cup[i]);
-                sum = sum % cup[i];
-            }
-
-        }
-        bank.close();
-        std::ofstream bankOut("bank.bin", std::ios::binary);
-        for (int i = 0; i < 6; i++)
-        {
-            if((rub[i] -= breakdown[i])<0);
-        }
-
-
+            std::cout << "The amount must be a multiple of 100\n";
     }
-
 }
